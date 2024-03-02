4fbfe0220d0605ed077a2f89d91cdc7042be6be2
Armanet Florian <armanet.florian@gmail.com>
2024-03-02 18:09:56 +0100

    Refacto

--- src/js/pages/Category.js
+++ src/js/pages/Category.js
@@ -1,50 +1,22 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { useLocation, useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import { STATUS_FAILED, STATUS_LOADING, STATUS_SUCCEEDED } from '../utils/constants'
+import { useParams, NavLink } from 'react-router-dom'
+import { useEffect } from 'react'
+import { PATH_CATEGORIES, STATUS_FAILED, STATUS_LOADING, STATUS_SUCCEEDED } from '../utils/constants'
 import fetchProductsOfCategory from '../api/productsOfCategory'
 import ProductMiniature from '../components/ListProducts/ProductMiniature'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import LoaderBlock from '../components/Loader/LoaderBlock'
-import { setCurrentProductsOfCategory } from '../store/productsOfCategoryStore'
 
 const Category = () => {
-    const location                                            = useLocation()
-    const dispatch                                            = useDispatch()
-    const params                                              = useParams()
-    const paramCategoryLabelOrigin                            = params.categoryLabelOrigin
-    const categories                                          = useSelector(state => state.categories.categories)
-    const currentCategory                                     = [...categories].find(cat => cat.categoryLabelOrigin === paramCategoryLabelOrigin)
-    const productsOfCategoryStatusRequest                     = useSelector(state => state.productsOfCategory.status)
-    const productsOfCategoryFetched                           = useSelector(state => state.productsOfCategory.productsOfCategoryFetched)
-    const currentProductsOfCategory                           = useSelector(state => state.productsOfCategory.currentProductsOfCategory)
-    const [contentFetchingProcess, setContentFetchingProcess] = useState(<p>En attente d\'une requête...</p>)
+    const dispatch = useDispatch()
+    const params = useParams()
+    const paramCategoryLabelOrigin = params.categoryLabelOrigin
+    const categories = useSelector(state => state.categories.categories)
+    const currentCategory = [...categories].find(cat => cat.categoryLabelOrigin === paramCategoryLabelOrigin)
+    const productsOfCategoryStatusRequest = useSelector(state => state.productsOfCategory.status)
+    const productsOfCategoryFetched = useSelector(state => state.productsOfCategory.productsOfCategoryFetched)
 
-    /**
-     *
-     */
-    useEffect(() => {
-        if (productsOfCategoryStatusRequest === STATUS_LOADING) {
-            dispatch(setCurrentProductsOfCategory([]))
-            setContentFetchingProcess(<LoaderBlock nbBlocks={ 8 }/>)
-            return
-        }
-
-        if (productsOfCategoryStatusRequest === STATUS_SUCCEEDED) {
-            setContentFetchingProcess(<></>)
-            return
-        }
-
-        if (productsOfCategoryStatusRequest === STATUS_FAILED) {
-            setContentFetchingProcess(
-                <p className="p-4 bg-tertiary-light/75 text-primary-dark font-bold">Echec de la requête !</p>
-            )
-            return
-        }
-
-        setContentFetchingProcess(<></>)
-
-    }, [productsOfCategoryStatusRequest, dispatch])
+    const currentProductsOfCategory = [...productsOfCategoryFetched].filter(product => product.category.categoryLabelOrigin === paramCategoryLabelOrigin)
 
     /**
      *
@@ -52,41 +24,48 @@ const Category = () => {
     useEffect(() => {
         if (!productsOfCategoryFetched.some(product => product.category.categoryLabelOrigin === paramCategoryLabelOrigin)) {
             dispatch(fetchProductsOfCategory(paramCategoryLabelOrigin))
-            return
         }
+    }, [])
 
-        dispatch(setCurrentProductsOfCategory([...productsOfCategoryFetched].filter(product => product.category.categoryLabelOrigin === paramCategoryLabelOrigin)))
-    }, [productsOfCategoryFetched, location, dispatch])
-
-    if (currentProductsOfCategory.length) {
+    if (productsOfCategoryStatusRequest === STATUS_SUCCEEDED) {
         return (
             <div>
-                <h1 className="text-center text-primary-light font-bold text-2xl lg:text-3xl mb-4 lg:mb-10">{ currentCategory.categoryLabel }</h1>
+                <NavLink to={PATH_CATEGORIES} className="Button Button--primary mb-8">
+                    <i className="Icon-arrow-left mr-2"></i>
+                    <span>Retour aux catégories</span>
+                </NavLink>
+                <h1 className="text-center text-primary-light font-bold text-2xl lg:text-3xl mb-4 lg:mb-10">{currentCategory.categoryLabel}</h1>
                 <TransitionGroup component="ul" className="flex flex-wrap justify-center lg:justify-start -mx-2">
-                    { currentProductsOfCategory.map(product => (
+                    {currentProductsOfCategory.map(product => (
                         <CSSTransition
-                            in={ true }
-                            key={ product.id }
-                            timeout={ 500 }
+                            in={true}
+                            key={product.id}
+                            timeout={500}
                             classNames="list"
                             unmountOnExit
                             appear
                         >
                             <li className="Card-product Card-product--sm">
-                                <ProductMiniature product={ product } key={ product.id }/>
+                                <ProductMiniature product={product} key={product.id} />
                             </li>
                         </CSSTransition>
-                    )) }
+                    ))}
                 </TransitionGroup>
             </div>
         )
     }
 
-    return (
-        <>
-            { contentFetchingProcess }
-        </>
-    )
+    if (productsOfCategoryStatusRequest === STATUS_LOADING || !productsOfCategoryStatusRequest) {
+        return (
+            <LoaderBlock nbBlocks={8} />
+        )
+    }
+
+    if (productsOfCategoryStatusRequest === STATUS_FAILED) {
+        return (
+            <p className="p-4 bg-tertiary-light/75 text-primary-dark font-bold">Echec de la requête !</p>
+        )
+    }
 }
 
 export default Category
